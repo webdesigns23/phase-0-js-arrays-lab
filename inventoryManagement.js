d5aa14610c1eecf09476eea37fc492dc6a94e159
Nick Christensen <nickachristensen@gmail.com>
2024-07-31 08:51:55 -0400

    Remove solution

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,48 +1,8 @@
-// Declare the Array
-let products = [
-    ["Laptop", "Electronics", 10, 999.99],
-    ["Phone", "Electronics", 25, 699.99],
-    ["Headphones", "Accessories", 50, 199.99],
-    ["Monitor", "Electronics", 15, 299.99]
-  ];
+// Write your code here
 
-// Access Product Information
-function logFirstProduct() {
-  console.log(products[0]);
-}
 
-// Modify Product Information
-function updateProductQuantity(product, newQuantity) {
-  product[2] = newQuantity;
-}
 
-// Check Product Availability
-function checkProductAvailability(product) {
-  if (product[2] > 0) {
-    console.log("Product is available");
-  } else {
-    console.log("Product is out of stock");
-  }
-}
-
-// Calculate Total Inventory Value
-function calculateTotalInventoryValue(products) {
-    let totalValue = 0;
-
-    for (let i = 0; i < products.length; i++) {
-      totalValue += products[i][2] * products[i][3];
-    }
-
-    totalValue = totalValue.toFixed(2);
-    console.log("Total Inventory Value: $" + totalValue);
-  }
-
-// Remove Product
-function removeLastProduct(){
-    products.pop();
-}
-
-// Export the necessary parts for testing
+// Export the necessary parts for testing. Don't modify.
 module.exports = {
     products,
     logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
