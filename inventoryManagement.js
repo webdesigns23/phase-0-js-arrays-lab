9e40ef09a50aa93be2d6cc0d712bc3b097c47e0d
Nicholas Christensen <nickachristensen@gmail.com>
2024-08-29 10:50:41 -0400

    Update inventoryManagement.js

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,14 +1,11 @@
 // Write your code here
 
 
-
 // Export the necessary parts for testing. Don't modify.
 module.exports = {
-    products,
-    logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-    updateProductQuantity: typeof updateProductQuantity !== 'undefined' ? updateProductQuantity : undefined,
-    checkProductAvailability: typeof checkProductAvailability !== 'undefined' ? checkProductAvailability : undefined,
-    removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-    calculateTotalInventoryValue: typeof calculateTotalInventoryValue !== 'undefined' ? calculateTotalInventoryValue : undefined
-  };
+  products,
+  logFirstProduct,
+  updateProductName,
+  removeLastProduct
+};
   
