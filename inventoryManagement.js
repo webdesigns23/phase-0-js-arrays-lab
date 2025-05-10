c0820750d75b47850b908a790fd14c411dc49124
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-05-10 06:01:50 -0600

    added array functions

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,12 +1,31 @@
-// Write your code here
+//create array
+const products = ["Laptop", "Phone", "Headphones", "Monitor"]
 
+//output first product
+function logFirstProduct() {
+    return products[0];
+} 
 
+//add product
+function addProduct(productName) {
+    return products.push();
+}
+
+//update product
+function updateProductName(index, newName) {
+    return products[index,newName];
+}
+
+//remove last product
+function removeLastProduct() {
+    return products.pop();
+  }
 
 // Export the necessary parts for testing
-module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
-};
+// module.exports = {
+//   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
+//   addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
+//   updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
+//   removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
+//   products
+// };
