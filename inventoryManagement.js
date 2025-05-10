51ca946fae229fbc469b575388b2825b4fe895f7
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-05-10 06:33:55 -0600

    fixed commented out, export the necessary parts for test section

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -22,10 +22,10 @@ function removeLastProduct() {
   }
 
 // Export the necessary parts for testing
-// module.exports = {
-//   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-//   addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-//   updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-//   removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-//   products
-// };
+module.exports = {
+  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
+  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
+  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
+  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
+  products
+};
